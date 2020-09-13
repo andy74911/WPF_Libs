4e4b4bd918e7cd79d3ff1637cae77282b7134f13
andy74911 <logical.chen@gmail.com>
2020-09-13 19:40:57 +0800

    First Version 1.With Column Header 2.HideSelectedListBoxItem

---
+++ DataGrid_ListBox/Model.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.Design;
+using System.ComponentModel;
+using System.Collections.ObjectModel;
+namespace DataGrid_ListBox.Model
+{
+    class Column_t : INotifyPropertyChanged
+    {
+        private ObservableCollection<Cell_t> _ocCell;
+        public ObservableCollection<Cell_t> ocCells
+        {
+            get { return _ocCell; }
+            set
+            {
+                if (_ocCell != value)
+                {
+                    this._ocCell = value;
+                    RaisePropertyChanged("ocCells");
+                }
+            }
+        }
+        public Column_t(int row, int column, int setColumn, double[][] model)
+        {
+            var oc = new ObservableCollection<Cell_t>();
+            for (int i = 0; i < row; i++)
+            {
+                oc.Add(new Cell_t(i, setColumn, model));
+            }
+            this.ocCells = oc;
+        }
+        public event PropertyChangedEventHandler PropertyChanged;
+        private void RaisePropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+    class Cell_t : INotifyPropertyChanged
+    {
+        internal int Column = 0;
+        internal int Row = 0;
+        private double[][] modelValue;
+        public Cell_t(int row, int column, double[][] modelValue)
+        {
+            this.Row = row;
+            this.Column = column;
+            this.modelValue = modelValue;
+        }
+        public string TextContent
+        {
+            get { return modelValue[Row][Column].ToString(); }
+            set
+            {
+                if (double.TryParse(value, out double d))
+                {
+                    if (d != modelValue[Row][Column])
+                    {
+                        modelValue[Row][Column] = d;
+                        RaisePropertyChanged("TextContent");
+                        Console.WriteLine("row:{0}, column:{1}, value:{2}", Row, Column, d);
+                    }
+                }
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        private void RaisePropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
---
+++ DataGrid_ListBox/ViewModel.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections.ObjectModel;
+using DataGrid_ListBox.Model;
+namespace DataGrid_ListBox.ViewModel
+{
+    class ViewModel_t
+    {
+        private ObservableCollection<Column_t> _ocColumn;
+        public ObservableCollection<Column_t> ocColumn
+        {
+            get { return _ocColumn; }
+            private set
+            {
+                if (_ocColumn != value)
+                    this._ocColumn = value;
+            }
+        }
+        private ObservableCollection<string> _ocRowHeader;
+        public ObservableCollection<string> ocRowHeader
+        {
+            get { return _ocRowHeader; }
+            private set
+            {
+                if (_ocRowHeader != value)
+                {
+                    _ocRowHeader = value;
+                }
+            }
+        }
+        public ViewModel_t()
+        {
+            int row = 5;
+            int column = 10;
+
+            var strs = (from i in Enumerable.Range(0, row+1)
+                        select "Row " + i).ToList();
+            this.ocRowHeader = new ObservableCollection<string>(strs);
+            double[][] modelValue = new double[row][];
+            for (int i = 0; i < row; i++)
+            {
+                modelValue[i] = new double[column];
+                for (int j = 0; j < column; j++)
+                {
+                    modelValue[i][j] = i * 100 + j;
+                }
+            }
+            var oc = new ObservableCollection<Column_t>();
+            for (int i = 0; i < column; i++)
+            {
+                oc.Add(new Column_t(row, column, i, modelValue));
+            }
+            this.ocColumn = oc;
+        }
+
+    }
+
+}
